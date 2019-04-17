b66711ba48513dfd4db2f83efbff1d8a08e94d48
Emmanuel Ojuba <mogbojuba@gmail.com>
2019-04-17 17:49:28 -0400

    Modify script for running ALS on MLLib

--- runMLLibALS.sh
+++ runMLLibALS.sh
@@ -3,11 +3,11 @@
 #SBATCH --job-name=proj_%j
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --partition=gpu
+#SBATCH --partition=general
 #SBATCH --mem=100Gb
 #SBATCH --export=ALL
 #SBATCH --time=24:00:00
 #SBATCH --output=/scratch/ojuba.e/Project/logs/exp_%j.out
 #SBATCH --error=/scratch/ojuba.e/Project/logs/exp_%j.err
 
-python -u UserFiltering.py --saveTo /scratch/ojuba.e/Project/results/baseResults_noNeg.csv
+python -u MatrixFactorizationALS.py --saveTo /scratch/ojuba.e/Project/results/baseResults2_noNeg.csv
